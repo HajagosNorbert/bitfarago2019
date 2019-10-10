df2223c0ca2ddc74f4140d2c181d9cf9c2028a0c
Andrew Black <50300348+0xAndrewBlack@users.noreply.github.com>
2019-10-10 12:05:14 +0200

    3. Fordulo elkezdve
    3. Fordulo "alapok"

---
+++ 3.resz/assets/script.js
@@ -0,0 +1,14 @@
+function start() {
+    const canvas = document.getElementById('cnvs');
+    const context = canvas.getContext('2d');
+
+    canvas.width = 500;
+    canvas.height = 500;
+
+    document.getElementById('fov').addEventListener("click", showFOV)
+}
+
+function showFOV() {
+
+}
+
