79a356e9c05e553932bc658e15b955dad206949c
0xAndrewBlack <>
2019-10-15 19:24:37 +0200

    X Y koordináták helyesen vannak 0-tól 500-ig, a loop rossz mert mindig az elsőt rajzolja de logolja a többit

--- 3.resz/assets/script.js
+++ 3.resz/assets/script.js
@@ -1,3 +1,13 @@
+const canvas = document.getElementById('cnvs');
+const ctx = canvas.getContext('2d');
+
+document.getElementById('fov').addEventListener("click", showFOV)
+
+const s1 = document.getElementById('sensor1pos');
+const s2 = document.getElementById('sensor2pos');
+const s3 = document.getElementById('sensor3pos');
+const s4 = document.getElementById('sensor4pos');
+
 function getSensorInformation() {
     var xhr = new XMLHttpRequest();
     xhr.onreadystatechange = function () {
@@ -5,6 +15,7 @@ function getSensorInformation() {
             if (this.status == 200) {
                 const response = JSON.parse(this.responseText);
                 sensors = response.data;
+                console.log(sensors);
                 drawSensPos(sensors);
                 /* sensors.forEach(sensor => {
                     drawSensPos(sensor);
@@ -18,26 +29,31 @@ function getSensorInformation() {
         request: 'sensors'
     }));
 }
+
 function showFOV() {
     console.log("Megnyomtad");
 }
-function drawDetectionArea(id, signal, angle){
-    if (signal){
+
+function drawDetectionArea(id, signal, angle) {
+    if (signal) {
         ctx.beginPath();
         ctx.moveTo(0, 0);
         ctx.lineTo(sensors[id].posx, sensors[id].posy);
         ctx.stroke();
-    } 
+    }
 }
 
 function drawSensPos(sensors) {
     for (var i = 0; i < sensors.length; i++) {
-        const x = sensors[i].posx;
-        const y = sensors[i].posy;
-        const a = sensors[i].angle;
+        x = sensors[i].posx;
+        y = sensors[i].posy;
+        a = sensors[i].angle;
+
         console.log(`${x} ${y} ${a}`);
+
         ctx.fillStyle = "#de1d1d";
         ctx.fillRect(x, y, 32, 32);
+
         ctx.rotate(a);
         ctx.restore();
     }
@@ -46,46 +62,36 @@ function drawSensPos(sensors) {
 let moveGlobalx = 0;
 let moveGlobaly = 0;
 function move(event) {
-    moveGlobalx = event.clientX;
-	moveGlobaly = event.clientY;
-	console.log(`Coords: ${moveGlobalx} ${moveGlobaly}`);
-	postMove();
+    const area = canvas.getBoundingClientRect();
+    moveGlobalx = event.clientX - area.left;
+    moveGlobaly = event.clientY - area.top;
+    console.log(`Coords: ${moveGlobalx} ${moveGlobaly}`);
+    postMove();
 }
-function postMove(){
-	var xhr = new XMLHttpRequest();
+
+function postMove() {
+    var xhr = new XMLHttpRequest();
     xhr.onreadystatechange = function () {
         if (this.readyState == 4 && this.status == 200) {
             if (this.status == 200) {
-				const response = JSON.parse(this.responseText);
-				console.log(response)
-				drawDetectionArea(id, signal, angle);
+                const response = JSON.parse(this.responseText);
+                console.log(response)
+                drawDetectionArea(response.id, response.signal, response.angle);
             }
         }
     };
     xhr.open('POST', 'http://bitkozpont.mik.uni-pannon.hu/Vigyazz3SensorData.php', true);
     xhr.setRequestHeader('Content-type', 'application/x-www-form-urlencoded');
     xhr.send(JSON.stringify({
-		request: 'sensordata',
-		version: '1',
-		posx: `${moveGlobalx}`,
-		posy: `${moveGlobaly}`
-	
-	}));
+        request: 'sensordata',
+        version: '1',
+        posx: `${moveGlobalx}`,
+        posy: `${moveGlobaly}`
+
+    }));
 }
 
 postMove();
-
 getSensorInformation();
-
-
-const canvas = document.getElementById('cnvs');
-const ctx = canvas.getContext('2d');
-
-const s1 = document.getElementById('sensor1pos');
-const s2 = document.getElementById('sensor2pos');
-const s3 = document.getElementById('sensor3pos');
-const s4 = document.getElementById('sensor4pos');
-
-document.getElementById('fov').addEventListener("click", showFOV)
 
 let sensors;
