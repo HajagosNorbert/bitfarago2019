5bd44e72432aef9da5e6c9c17b817961ae02375f
Andrew Black <50300348+0xAndrewBlack@users.noreply.github.com>
2019-10-15 11:28:36 +0200

    Poziciok x y egérrel

--- 3.resz/assets/script.js
+++ 3.resz/assets/script.js
@@ -1,16 +1,23 @@
 function start() {
-    
+
 }
 
 function showFOV() {
     console.log("Megnyomtad");
-    
+
 }
 
-class Point{
-    constructor(xPos, yPos){
-        
+class Point {
+    constructor(xPos, yPos) {
+
     }
+}
+
+function move(event) {
+    const x = event.clientX;
+    const y = event.clientY;
+
+    console.log(`Coords: ${x} ${y}`);
 }
 
 document.getElementById('fov').addEventListener("click", showFOV)
